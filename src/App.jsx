c326991a11d65b4c04fb16405bee7a93ffd62971
MaxTrgb <maksym.tryhub@gmail.com>
2024-07-01 17:19:10 +0300

    todo css

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div>
       {/* <Example /> */}
-      {/* <TodoList /> */}
-      <ProductGrid />
+      <TodoList />
+      {/* <ProductGrid /> */}
     </div>
   );
 }
