729fcc56b6a89afebe7cd248e37038b60019706c
MaxTrgb <maksym.tryhub@gmail.com>
2024-07-17 12:15:31 +0300

    sort works change view works

--- src/components/productGrid/productInfo.js
+++ src/components/productGrid/productInfo.js
@@ -2,8 +2,8 @@ export const productInfo = [
     {
         id: 1,
         imgSrc: "https://content2.rozetka.com.ua/goods/images/big_tile/446854857.jpg",
-        title: "Моноблок Lenovo 24 AIO V50a-24",
-        price: "20 999₴",
+        title: "1 Моноблок Lenovo 24 AIO V50a-24",
+        price: "3",
         details: "Екран 23.8 IPS (1920x1080) Full HD / Intel Core i5-10400T (2.0 - 3.6 ГГц) / RAM 8 ГБ / SSD 256 ГБ" +
             "/Intel UHD Graphics 630 / без ОД / LAN / веб-камера / Windows 10 Pro (upgrade to Windows 11)",
         items: 50
@@ -11,8 +11,8 @@ export const productInfo = [
     {
         id: 2,
         imgSrc: "https://content.rozetka.com.ua/goods/images/big_tile/320078659.jpg",
-        title: "Моноблок Apple iMac 24 М1 4.5К",
-        price: "26 999₴",
+        title: "2 Моноблок Apple iMac 24 М1 4.5К",
+        price: "1",
         details: "AMD Ryzen 5 5500 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / " +
             "nVidia GeForce RTX 3060, 12 ГБ / без ОД / LAN / без ОС",
         items: 10
@@ -20,8 +20,8 @@ export const productInfo = [
     {
         id: 3,
         imgSrc: "https://content1.rozetka.com.ua/goods/images/big_tile/176926071.jpg",
-        title: "Компютер ARTLINE Gaming X47",
-        price: "44 999₴",
+        title: "3 Компютер ARTLINE Gaming X47",
+        price: "2",
         details: "Екран 23.5 (4480x2520) 4.5K / Apple M1 / RAM 8 ГБ / SSD 256 ГБ / Apple M1 Graphics (7 ядер)" +
             "/ без ОД / Wi-Fi / Bluetooth / вебкамера / macOS Big Sur / 4.46 кг / синій / клавіатура + миша",
         items: 0
