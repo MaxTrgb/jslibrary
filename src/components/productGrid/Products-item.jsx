2a0142f9409b1f5839a40b366ced3093e6bbfd73
MaxTrgb <maksym.tryhub@gmail.com>
2024-08-20 13:43:25 +0300

    finish but small fixing

--- src/components/productGrid/Products-item.jsx
+++ src/components/productGrid/Products-item.jsx
@@ -54,7 +54,7 @@ const ProductsItem = ({ id, imgSrc, title, price, details, items }) => {
                 <img src={imgSrc} alt="" />
                 <h3>{title}</h3>
                 <p id='price'>{price} $</p>
-                <p id='check'><CheckAvailable items={items} /><button>Add to cart</button></p>
+                <p id='check'><CheckAvailable items={items} /></p>
                 <p className={`productDetails ${hovered ? 'hovered' : ''}`}>{details}</p>                
             </div>
 
